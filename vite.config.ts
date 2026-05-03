884c8cecaa6c369664029b6780789bf5c1d4ed30
Get Tshibanda <62560899+get174@users.noreply.github.com>
2026-05-03 19:11:55 +0100

    correction des vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __SUPABASE_URL__: JSON.stringify(import.meta.env.VITE_SUPABASE_URL || 'MISSING'),
-    __SUPABASE_ANON_KEY__: JSON.stringify(import.meta.env.VITE_SUPABASE_ANON_KEY || 'MISSING'),
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
