7e8b549903022d678416897a2572d1114f4c8154
Get Tshibanda <62560899+get174@users.noreply.github.com>
2026-06-19 18:28:13 +0100

    nouvelle lien

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const DOWNLOAD_URL = 'https://github.com/get174/glockcleaner/releases/tag/v1.1';
+export const DOWNLOAD_URL = 'https://github.com/get174/glockcleaner/releases/download/v1.1/GlockCleaner.exe';
